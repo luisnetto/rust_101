d7675cff9dbd46d0b967ea275f8bdb21426e92aa
Luís Netto <luisnttt@gmail.com>
2025-01-04 18:38:53 -0300

    Slices part 1

---
+++ slices_1.rs
@@ -0,0 +1,22 @@
+// function to return size of first word of string.
+fn first_word(s: &String) -> usize {
+    // transform the string into an array of bytes
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    
+    s.len()
+
+}
+
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+    println!("{}", word);
+    s.clear();
+}
